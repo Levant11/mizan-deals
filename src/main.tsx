b3a955abc3dcd4d620f9ba9f59f1e3d9d9034f3f
Levant <rafi@levant-bridge.com>
2026-04-25 02:01:15 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "@/lib/auth";
-import { Toaster } from "sonner";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-        <Toaster position="top-center" richColors />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
